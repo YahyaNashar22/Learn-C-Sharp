58b096928c5971fb273f9695d98aa60b4762c201
YahyaNashar22 <yahyanashar22@gmail.com>
2024-12-07 13:16:17 +0200

    Generic classes and methods

---
+++ GenericClassesAndDelegates/GenericClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClassesAndDelegates
+{
+    internal class GenericClass<T>
+    {
+        public GenericClass(T field, T value ) { this.Field = field; this.Value = value; }
+
+        #region Properties
+        public T Field { get; set; }
+        public T Value { get; set; }
+        #endregion
+
+
+        public void ShowType() => Console.WriteLine($"The type of the field {Field} is {Field.GetType()}");
+
+        public void Addition() => Console.WriteLine($"The addidtion of {Field} and {Value} is --> {(dynamic) Field + Value}");
+
+
+    }
+}
---
+++ GenericClassesAndDelegates/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace GenericClassesAndDelegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("--Generic Classes And Delegates--");
+            Console.WriteLine("---------------------------------");
+
+            GenericClass<int> firstClass = new GenericClass<int>(23, 10);
+            GenericClass<char> secondClass = new GenericClass<char>('C', 'D');
+
+            firstClass.ShowType();
+            secondClass.ShowType();
+
+            firstClass.Field = 10;
+            secondClass.Field = 'A';
+
+            firstClass.ShowType();
+            secondClass.ShowType();
+
+
+            firstClass.Addition();
+            secondClass.Addition();
+
+            ShowValueAndType(1);
+
+            void ShowValueAndType<TVariable>(TVariable myVariable)
+            {
+                Console.WriteLine($"The value of my variable is {myVariable} and the type is {myVariable.GetType()} ");
+            }
+
+        }
+    }
+}
