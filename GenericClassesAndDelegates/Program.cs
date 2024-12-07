af4558abe05f77501e585b2e4f766a6b91a3bc23
YahyaNashar22 <yahyanashar22@gmail.com>
2024-12-07 16:23:21 +0200

    callback methods

--- GenericClassesAndDelegates/Program.cs
+++ GenericClassesAndDelegates/Program.cs
@@ -8,29 +8,47 @@
             Console.WriteLine("--Generic Classes And Delegates--");
             Console.WriteLine("---------------------------------");
 
-            GenericClass<int> firstClass = new GenericClass<int>(23, 10);
-            GenericClass<char> secondClass = new GenericClass<char>('C', 'D');
+            #region Generic Classes
 
-            firstClass.ShowType();
-            secondClass.ShowType();
+            //GenericClass<int> firstClass = new GenericClass<int>(23, 10);
+            //GenericClass<char> secondClass = new GenericClass<char>('C', 'D');
 
-            firstClass.Field = 10;
-            secondClass.Field = 'A';
+            //firstClass.ShowType();
+            //secondClass.ShowType();
 
-            firstClass.ShowType();
-            secondClass.ShowType();
+            //firstClass.Field = 10;
+            //secondClass.Field = 'A';
+
+            //firstClass.ShowType();
+            //secondClass.ShowType();
 
 
-            firstClass.Addition();
-            secondClass.Addition();
+            //firstClass.Addition();
+            //secondClass.Addition();
 
-            ShowValueAndType(1);
+            //ShowValueAndType(1);
 
-            void ShowValueAndType<TVariable>(TVariable myVariable)
+            //void ShowValueAndType<TVariable>(TVariable myVariable)
+            //{
+            //    Console.WriteLine($"The value of my variable is {myVariable} and the type is {myVariable.GetType()} ");
+            //}
+
+            #endregion
+
+
+            #region Callback methods
+            void ShowResult(int result) => Console.WriteLine($"The Result is --> {result}");
+
+            void Sum (int x, int y, Action<int> ShowResult)
             {
-                Console.WriteLine($"The value of my variable is {myVariable} and the type is {myVariable.GetType()} ");
+                int result = x + y;
+                ShowResult(result);
             }
 
+
+            Sum(1, 3, ShowResult);
+
+            #endregion
         }
     }
 }
