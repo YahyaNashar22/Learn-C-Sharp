f2f7184795b969f390e3ec7016fe0629133cd2a5
YahyaNashar22 <yahyanashar22@gmail.com>
2024-12-30 10:57:42 +0200

    arrays

---
+++ PhraseItBackward/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PhraseItBackward
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] phrase = new string[4];
+
+            for ( int i = 0; i < phrase.Length; i++ )
+            {
+                Console.WriteLine($"Enter the #{i + 1} word");
+                phrase[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("---------------------------");
+
+            for ( int i = phrase.Length - 1; i >= 0; i-- )
+            {
+                Console.WriteLine(phrase[i]);
+            }
+        }
+    }
+}
