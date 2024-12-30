6e68807561dc246877efb7d374521853794f9883
YahyaNashar22 <yahyanashar22@gmail.com>
2024-12-30 11:11:09 +0200

    array number calculator

--- PhraseItBackward/Program.cs
+++ PhraseItBackward/Program.cs
@@ -4,20 +4,54 @@
     {
         static void Main(string[] args)
         {
-            string[] phrase = new string[4];
+            #region phrase
+            //string[] phrase = new string[4];
 
-            for ( int i = 0; i < phrase.Length; i++ )
+            //for ( int i = 0; i < phrase.Length; i++ )
+            //{
+            //    Console.WriteLine($"Enter the #{i + 1} word");
+            //    phrase[i] = Console.ReadLine();
+            //}
+
+            //Console.WriteLine("---------------------------");
+
+            //for ( int i = phrase.Length - 1; i >= 0; i-- )
+            //{
+            //    Console.WriteLine(phrase[i]);
+            //}
+
+            #endregion
+
+
+            #region addNumbers
+
+            int[] numbers = new int[4];
+            bool success;
+            int sum = 0;
+
+            Console.WriteLine("Enter the numbers you want to add");
+            Console.WriteLine("---------------------------------");
+
+            for (int i = 0; i < numbers.Length; i++)
             {
-                Console.WriteLine($"Enter the #{i + 1} word");
-                phrase[i] = Console.ReadLine();
+                Console.WriteLine($"Enter the #{i + 1} number:");
+                success = int.TryParse(Console.ReadLine(), out numbers[i]);
+                if (!success)
+                {
+                    Console.WriteLine("Invalid integer");
+                }
             }
 
-            Console.WriteLine("---------------------------");
 
-            for ( int i = phrase.Length - 1; i >= 0; i-- )
+            for (int i = 0; i < numbers.Length; i++)
             {
-                Console.WriteLine(phrase[i]);
+                sum += numbers[i];
             }
+
+            Console.WriteLine("The Total Sum of all Numbers is: " + sum);
+
+
+            #endregion
         }
     }
 }
