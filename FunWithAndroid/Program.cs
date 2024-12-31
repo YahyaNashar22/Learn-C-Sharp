f06de83af1c1ef2e3edf5f073a04d554cc7a070f
YahyaNashar22 <yahyanashar22@gmail.com>
2024-12-31 10:57:11 +0200

    trying to build console for android

---
+++ FunWithAndroid/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FunWithAndroid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string enteredNumber;
+
+
+            Console.WriteLine("Hello, World From Android!");
+            Console.WriteLine("If this is running on your phone it means mobile dev ain't that hard after all!");
+
+            Console.WriteLine("---------------------------------------");
+
+            Console.Write("Enter a number:");
+            enteredNumber = Console.ReadLine();
+
+            Console.WriteLine("You are gay!");
+
+            Console.ReadKey();
+        }
+    }
+}
