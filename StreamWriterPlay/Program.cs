7d7be21648b21414b751ee8d05c5bf901974c0b2
YahyaNashar22 <yahyanashar22@gmail.com>
2025-01-04 00:03:14 +0200

    implemented streamWriter

---
+++ StreamWriterPlay/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace StreamWriterPlay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter streamWriter = new StreamWriter("output.txt");
+
+            string input;
+
+            Console.WriteLine("Enter desired text below:");
+            Console.WriteLine("-------------------------");
+
+            input = Console.ReadLine();
+
+            streamWriter.WriteLine(input);
+
+            streamWriter.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
