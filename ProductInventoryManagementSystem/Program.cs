c0948d4b3a553474e69e227bc65590dbca1272d6
YahyaNashar22 <yahyanashar22@gmail.com>
2025-01-04 01:31:41 +0200

    started program inventory management

---
+++ ProductInventoryManagementSystem/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ProductInventoryManagementSystem
+{
+    enum Screens
+    {
+        Main,
+        AddItem,
+        ViewItems,
+        DeleteItem,
+    }
+
+
+    internal class Program
+    {
+       static Screens screen = Screens.Main;
+
+        static void Main(string[] args)
+        {
+        }
+
+        static void MainMenu()
+        {
+            Console.WriteLine("Welcome To The Product Inventory Management System!");
+            Console.WriteLine("How can we help you?");
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("[1] - Add Item");
+            Console.WriteLine("[2] - View Items");
+            Console.WriteLine("[3] - Delete Item");
+
+            int answer = int.Parse(Console.ReadLine());
+            if (answer == 1)
+            {
+               screen = Screens.AddItem;
+            }
+        }
+    }
+}
