4bb276566b09dc7d8f2003fb931c0d5478a8ef99
YahyaNashar22 <yahyanashar22@gmail.com>
2025-01-04 00:35:11 +0200

    added streamReader

--- StreamWriterPlay/Program.cs
+++ StreamWriterPlay/Program.cs
@@ -7,15 +7,48 @@
             StreamWriter streamWriter = new StreamWriter("output.txt");
 
             string input;
-
+            string fromFile;
+            string answer;
+            
             Console.WriteLine("Enter desired text below:");
             Console.WriteLine("-------------------------");
+            Console.WriteLine();
 
             input = Console.ReadLine();
 
             streamWriter.WriteLine(input);
 
             streamWriter.Close();
+
+            Console.WriteLine("Do you want to add something else?");
+
+            answer = Console.ReadLine();
+
+            if (answer == "y")
+            {
+                streamWriter = new StreamWriter("output.txt", true);
+                Console.WriteLine("Enter desired text below:");
+                Console.WriteLine("-------------------------");
+                Console.WriteLine();
+
+                input = Console.ReadLine();
+
+                streamWriter.WriteLine(input);
+
+                streamWriter.Close();
+            }
+
+
+            StreamReader streamReader = new StreamReader("output.txt");
+
+            fromFile = streamReader.ReadToEnd();
+
+            Console.WriteLine("You Entered:");
+            Console.WriteLine("------------");
+            Console.WriteLine();
+            Console.WriteLine(fromFile);
+
+            streamReader.Close();
 
             Console.ReadKey();
         }
