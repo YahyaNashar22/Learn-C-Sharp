4a703cb210babf7f66bb9d29ac5aa569573ac3f8
YahyaNashar22 <yahyanashar22@gmail.com>
2025-01-04 11:30:28 +0200

    Added AddItems and ViewItems methods

--- ProductInventoryManagementSystem/Program.cs
+++ ProductInventoryManagementSystem/Program.cs
@@ -11,10 +11,22 @@
 
     internal class Program
     {
-       static Screens screen = Screens.Main;
+        static Screens screen = Screens.Main;
+        static List<string> productNames = new List<string>();
+        static List<double> productPrices = new List<double>();
+
 
         static void Main(string[] args)
         {
+            while (true)
+            {
+                switch (screen)
+                {
+                    case Screens.Main:  Console.Clear();  MainMenu(); break;
+                    case Screens.AddItem: Console.Clear();  AddItemsScreen(); break;
+                    case Screens.ViewItems: Console.Clear();  ViewItemsScreen(); break;
+                }
+            }
         }
 
         static void MainMenu()
@@ -27,11 +39,60 @@
             Console.WriteLine("[1] - Add Item");
             Console.WriteLine("[2] - View Items");
             Console.WriteLine("[3] - Delete Item");
+            Console.WriteLine("[4] - Save & Quit");
 
+
+            int answer = int.Parse(Console.ReadLine());
+            switch (answer)
+            {
+                case (1): screen = Screens.AddItem; break;
+                case (2): screen = Screens.ViewItems; break;
+                case (3): screen = Screens.DeleteItem; break;
+                case (4): Environment.Exit(0); break;
+            }
+
+        }
+
+        static void AddItemsScreen()
+        {
+            string productName;
+            double productPrice;
+
+            while (true)
+            {
+                Console.Write("Enter the product's name:");
+                productName = Console.ReadLine();
+                if (productName == "")
+                {
+                    screen = Screens.Main;
+                    break;
+                }
+                else
+                {
+                    Console.Write("Enter the product's price:");
+                    productPrice = double.Parse(Console.ReadLine());
+
+                    productNames.Add(productName);
+                    productPrices.Add(productPrice);
+                }
+
+            }
+        }
+
+        static void ViewItemsScreen()
+        {
+            Console.WriteLine("Available item in the inventory:");
+
+            for (int i = 0; i < productNames.Count; i++)
+            {
+                Console.WriteLine($"{productNames[i]} - {productPrices[i]}$");
+            }
+
+            Console.WriteLine("[1] - Go Back");
             int answer = int.Parse(Console.ReadLine());
             if (answer == 1)
             {
-               screen = Screens.AddItem;
+                screen = Screens.Main;
             }
         }
     }
