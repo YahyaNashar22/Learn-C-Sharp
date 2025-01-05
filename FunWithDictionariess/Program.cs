744ebafad64326825942b8a75e0ef2e16e71ccf2
YahyaNashar22 <yahyanashar22@gmail.com>
2025-01-05 13:21:27 +0200

    created a simple dictionary to play with

---
+++ FunWithDictionariess/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace FunWithDictionariess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> persons = new Dictionary<string, string>();
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("| Welcome To Dictionaries! |");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine();
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("add <person>");
+                Console.WriteLine("describe <person>");
+                Console.WriteLine();
+
+                string input = Console.ReadLine();
+                var parts = input.Split(" ");
+
+                if (parts[0] == "add")
+                {
+                    Console.WriteLine($"describe {parts[1]}:");
+                    string description = Console.ReadLine();
+
+                    persons.Add(parts[1], description);
+                }
+
+                if (parts[0] == "describe")
+                {
+                    if (!persons.TryGetValue(parts[1], out var person))
+                    {
+                        Console.WriteLine($"No description available for {parts[1]}");
+                        continue;
+                    }
+                    Console.WriteLine($"{parts[1]} is {persons[parts[1]]}");
+                }
+            }
+        }
+    }
+}
