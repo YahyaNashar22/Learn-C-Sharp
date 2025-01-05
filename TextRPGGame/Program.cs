e57af06109a707577bb93ec5a6a9c114d5550a53
YahyaNashar22 <yahyanashar22@gmail.com>
2025-01-05 11:59:40 +0200

    Added the basic instructions for the start of the rpg game

---
+++ TextRPGGame/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * REQUIREMENTS:
+ * -------------
+ * 
+ * A- States:
+ * Main Menu State
+ * Load File State
+ *      - Allow the user to choose a file given a list of files to choose from
+ *  
+ *  Save File State:
+ *      - Ask you for a file name
+ *      - Save an EMPTY TEXT given the provided name
+ * 
+ * New Game State
+ * 
+ * 
+ * B- Commands:
+ * Switch State Command
+ * Help Command
+ * Load File Command
+ * Save File Command
+ * New Game Command
+ * 
+ * 
+ * Note: For all of the states allow the user to return to the main menu
+ * Note: New Game will be a dead end -- Console.WriteLine("NEW GAME!");
+ */
+
+
+
+
+
+
+
+
+namespace TextRPGGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
