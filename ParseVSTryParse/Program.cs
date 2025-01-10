8da3d0e3c87059479f2087bb653bbb5e97703582
YahyaNashar22 <yahyanashar22@gmail.com>
2025-01-10 15:40:38 +0200

    created a small prgrm to demonstrate the difference between parse and tryParse

---
+++ ParseVSTryParse/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ParseVSTryParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int parsedInput;
+
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Hello, Wayyy!");
+            Console.WriteLine("Welcome to Parse vs TryParse lesson!!!");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine();
+
+
+            Console.Write("Enter you number:");
+            input = Console.ReadLine();
+
+            #region tryCatch
+            try
+            {
+                parsedInput = int.Parse(input);
+                Console.WriteLine("Parsed Correctly!");
+            }
+            catch
+            {
+                Console.WriteLine("Unable To Parse");
+            }
+            #endregion
+
+            #region TryParse
+            if (int.TryParse(input, out parsedInput))
+            {
+                Console.WriteLine("Parsed Correctly!");
+            }
+            else
+            {
+                Console.WriteLine("Unable To Parse");
+            }
+            #endregion
+        }
+    }
+}
