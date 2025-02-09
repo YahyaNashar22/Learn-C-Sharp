00f0c818a597b1ab3055f596dbebe138eb567e8f
YahyaNashar22 <yahyanashar22@gmail.com>
2025-02-09 11:43:30 +0200

    discovered hashsets

---
+++ HashSet/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace HashSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("--------- Welcome To HashSets ---------");
+            Console.WriteLine("---------------------------------------");
+
+
+            HashSet<int> set = new();
+
+            set.Add(1);
+            set.Add(2);
+            set.Add(1);
+
+            Console.WriteLine($"set.Add(1) evaluates to false because the HashSet already contains the value 1: {set.Add(1)}");
+
+            foreach (int item in set)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
